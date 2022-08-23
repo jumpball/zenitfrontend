e57a8b0895f1bf37ab973c59d005324e3f4f3de4
Ivan Kucherov <kucher_up@mail.ru>
2022-08-23 18:37:44 +0300

    7.1

--- src/js/files/blog-slider.js
+++ src/js/files/blog-slider.js
@@ -242,6 +242,82 @@ var swiper = new Swiper('.banner-slider', {
 	}
 });
 
+// === PARTNERS SLIDER === //
+
+var swiper = new Swiper('.partners-slider', {
+	slidesPerView: 'auto',
+	//slidesPerView: 3,
+	spaceBetween: 20,
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+	freeMode: true,
+	loop: true,
+	slideToClickedSlide: true,
+	//autoHeight: true,
+	autoplay: {
+		delay: 7000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+	},
+	//centeredSlides: true,
+	
+	
+	// effect: 'coverflow',
+	// coverflowEffect: {
+	// rotate: 15,
+	// stretch: 150,
+	// slideShadows: true,
+	// },
+
+	mousewheel: {
+		invert: false,
+	},
+	// pagination: {
+	// 	el: '.swiper-pagination',
+	// 	type: 'bullets',
+	// 	clickable: true,
+		//dynamicBullets:true,
+		
+	//},
+	navigation: {
+		prevEl: '.banner-slider__nav-prev',
+		nextEl: '.banner-slider__nav-next',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+			470: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+			690: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		910: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+		1140: {
+			slidesPerView: 5,
+			spaceBetween: 20,
+		},
+		1360: {
+			slidesPerView: 6,
+			spaceBetween: 20,
+		},
+	}
+});
+
+
+
+
+
+
+
 // === BANNER SLIDER-VERT === //
 
 var swiper = new Swiper('.banner-slider-vert', {
