aab12e9e1b85be11e90fb8d9c330df6ec5f3b83f
Ivan Kucherov <kucher_up@mail.ru>
2022-08-25 02:13:23 +0300

    9.6

--- src/js/files/publication-selector.js
+++ src/js/files/publication-selector.js
@@ -1,14 +1,18 @@
 // === PUBLICATIONS SELECTOR === //
 let boxs = document.querySelectorAll(".publication-selector__item");
 document.querySelector(".publication-selector__menu").addEventListener("click", function (event) {
-  if (event.target.tagName != "BUTTON") return false;
+	if (event.target.tagName != "BUTTON") return false;
 
-  let target = event.target.dataset["view"];
+	let target = event.target.dataset["view"];
 
-  boxs.forEach(function (el) {
-    el.classList.remove("hide");
-    if (!el.classList.contains(target) && target != "pub-all") {
-      el.classList.add("hide");
-    }
-  });
+	boxs.forEach(function (el) {
+		el.classList.remove("hide");
+		if (!el.classList.contains(target) && target != "pub-all") {
+			el.classList.add("hide");
+		}
+	});
 });
+
+
+
+// ================== PUBLICATIONS SELECTOR ============================ //
